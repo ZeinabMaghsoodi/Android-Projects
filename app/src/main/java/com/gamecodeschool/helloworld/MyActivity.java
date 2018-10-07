13b08a0396710c5da96ce4520f8f96695962baa1
ZeinabMaghsoodi <maghsoodi_zf@yahoo.com>
2018-10-07 14:19:01 +0330

    initial commit

---
+++ app/src/main/java/com/gamecodeschool/helloworld/MyActivity.java
@@ -0,0 +1,17 @@
+package com.gamecodeschool.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_layout);
+        Toast.makeText(this,"Hello Everybody...",Toast.LENGTH_LONG).show();
+    }
+}
